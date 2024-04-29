c8f5c15e3dbd3458d42b81e2d95f370ed357c8e2
Ahmed T.Nabarawy <iNabarawy@gmail.com>
2024-04-29 22:24:08 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ahmed T.Nabarawy on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
